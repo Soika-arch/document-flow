500c61187b202b2b9ecac524ae6fcd5ddd2f5a1a
Zorkiy <vladimirovichser@gmail.com>
2024-06-09 13:04:58 +0300

    Update sync_ftp.sh

--- service/sync_ftp.sh
+++ service/sync_ftp.sh
@@ -7,6 +7,6 @@ LOCAL_DIR='/var/www/html/document-flow.loc/'
 REMOTE_DIR='/notes.petamicr.zzz.com.ua'
 
 lftp -u $USER,$PASS $HOST << EOF
-mirror --reverse --delete --verbose $LOCAL_DIR $REMOTE_DIR
+mirror --reverse --delete --verbose --exclude ".git" --exclude "service" --exclude "README.md" --exclude ".gitignore" --exclude "test.sql" --exclude ".editorconfig" --exclude ".vscode" --exclude ".php81" $LOCAL_DIR $REMOTE_DIR
 quit
 EOF
